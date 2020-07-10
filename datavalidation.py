6b43797cf9aa69b3a73a6cee4700e2d764dddda2
sshang1995 <46602526+sshang1995@users.noreply.github.com>
2020-07-10 14:59:27 -0400

    Add python file

---
+++ datavalidation.py
@@ -0,0 +1,38 @@
+import pyodbc
+import pandas as pd
+
+# connect to SQL server
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=Saa01sql02;'
+                      'Databse=PortalCustom;'
+                      'Trusted_Connection=yes;')
+                      
+cursor = conn.cursor()
+# Please Change Stored Procedure name when you run code.
+cursor.execute('Call [sandbox].[AE_Fee_Data](?)',('11/1/2018'))
+old_data = []
+for row in cursor:
+  old_data.append(row)
+print("old DWH the number of rows: " + str(len(old_data)))
+
+# connect to second SQL server
+conn2 = pyodbc.connect('Driver={SQL Server};'
+                      'Server=Saa01sql02;'
+                      'Databse=PortalCustom;'
+                      'Trusted_Connection=yes;')
+                      
+cursor2 = conn2.cursor()
+# Please Change Stored Procedure name when you run code.
+cursor.execute('Call [dataset].[AE_Fee_Data](?)',('11/1/2018'))
+new_data = []
+for row in cursor2:
+  new_data.append(row)
+print("new DWH the number of rows: " + str(len(new_data)))
+
+# data comparison
+diffdata =[diff for diff in new_data if diff not in old_data]
+df = pd.DataFrame(diffdata)
+print("unmatching data in new DWH the number of rows: "+str(len(df)))
+
+# save result to txt file
+df.to_csv(r'C:\Users\c_ssshang\Desktop\result\AE_Fee_Data.txt', sep=' ', index=False)
